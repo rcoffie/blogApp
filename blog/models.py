8392832a31f45dd24543702f351b32a13c2a7a12
Robert Coffie <rcoffie22@yahoo.com>
2020-12-08 21:59:05 +0000

    search query

--- blog/models.py
+++ blog/models.py
@@ -12,13 +12,18 @@ class Post(models.Model):
     ('Fashion','Fashion'),
     ('Technology','Technology')
   )
+  Status=(
+    ('Draft','Draft'),
+    ('Published','Published')
+  )
 
 
   title = models.CharField(max_length=100)
   author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-  category = models.CharField(max_length=100,choices=Category)
+  category = models.CharField(max_length=100,choices=Category,null=True)
   body = models.TextField()
   featured_post = models.BooleanField(default=False)
+  # status = models.CharField(max_length=100,choices=Status)
   blog_image = models.ImageField(upload_to='photos/%Y/%m/%d')
   date_posted = models.DateField(default=datetime.now)
 
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,9 @@ from .models import *
 def Home(request):
   featured_post = Post.objects.all().filter(featured_post=True)[:1]
   posts = Post.objects.all()
+  query = request.GET.get('q')
+  if query:
+    posts=Post.objects.all().filter(title__icontains=query)
   context = {'posts':posts,'featured_post':featured_post}
   return render(request,'blog/index.html',context)
 
@@ -14,6 +17,7 @@ def Home(request):
 
 def PostDetail(request, id):
   post = Post.objects.get(id=id)
+  
   context = {'post':post}
 
   return render(request,'blog/detail.html',context)
